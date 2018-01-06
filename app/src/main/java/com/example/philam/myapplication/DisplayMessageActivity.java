2a550c34f4cf6577cddba3bb4a0a1c01c9d384c5
Phi <phi.lam64@gmail.com>
2018-01-06 13:04:38 -0800

    initial commit

---
+++ app/src/main/java/com/example/philam/myapplication/DisplayMessageActivity.java
@@ -0,0 +1,44 @@
+package com.example.philam.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        // Find the ListView resource.
+        //mainListView = (ListView) findViewById( R.id.mainListView );
+
+        // Create and populate a List of planet names.
+        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
+                "Jupiter", "Saturn", "Uranus", "Neptune"};
+        ArrayList<String> planetList = new ArrayList<String>();
+        planetList.addAll( Arrays.asList(planets) );
+
+        // Create ArrayAdapter using the planet list.
+        //listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, planetList);
+
+        // Add more planets. If you passed a String[] instead of a List<String>
+        // into the ArrayAdapter constructor, you must not add more items.
+        // Otherwise an exception will occur.
+        /*listAdapter.add( "Ceres" );
+        listAdapter.add( "Pluto" );
+        mainListView.setAdapter( listAdapter );
+        listAdapter.add( "Haumea" );
+        listAdapter.add( "Makemake" );
+        listAdapter.add( "Eris" );
+
+        // Set the ArrayAdapter as the ListView's adapter. */
+    }
+}
---
+++ app/src/main/java/com/example/philam/myapplication/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.philam.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.button5);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    private class MyAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return Cheeses.CHEESES.length;
+        }
+
+        public int getDate() {
+        }
+
+
+
+        @Override
+        public String getItem(int position) {
+            return Cheeses.CHEESES[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return Cheeses.CHEESES[position].hashCode();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup container) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.list_item, container, false);
+            }
+
+            ((TextView) convertView.findViewById(android.R.id.text1))
+                    .setText(getItem(position));
+            return convertView;
+        }
+    }
+}
+
